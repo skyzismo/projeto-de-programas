a50faca06f13486d0fe430b5f4e157bb80015270
skyzismo <lucas.petigato@hotmail.com>
2025-06-24 19:05:00 -0400

    feat/added initial design improvements

--- TP1-Implementacao/src/br/ufam/icomp/reservas/Main.java
+++ TP1-Implementacao/src/br/ufam/icomp/reservas/Main.java
@@ -1,9 +1,22 @@
 package br.ufam.icomp.reservas;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Configura o Look and Feel antes de criar a interface
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            // Configurações padrão
+            UIManager.put("Button.background", new Color(163, 178, 184));
+            // UIManager.put("Button.foreground", Color.WHITE);
+            UIManager.put("Button.font", new Font("Segoe UI", Font.BOLD, 12));
+            UIManager.put("Label.font", new Font("Segoe UI", Font.PLAIN, 12));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         SwingUtilities.invokeLater(() -> new InterfacePrincipal().setVisible(true));
     }
 }
